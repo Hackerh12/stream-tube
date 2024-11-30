d82492a198d6b4fe5887f3e27bd822763c5b451c
GOOSE <s.heroku@yahoo.com>
2024-12-01 01:38:18 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true)
+// Enable strict query filtering for Mongoose
+mongoose.set('strictQuery', true);
 
 const DBconnection = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI).catch((err) => {
-		console.log(`For some reasons we couldn't connect to the DB`.red, err)
-	})
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
-	console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
-}
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (err) {
+        console.error(`Database connection failed: ${err.message}`.red.bold);
+        process.exit(1); // Exit process with failure
+    }
+};
 
-module.exports = DBconnection
+module.exports = DBconnection;
